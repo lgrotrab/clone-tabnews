bb9e305f493387eedb83d293a49f858c1ce46196
Luis Gustavo Rodrigues Oliveira <54413345+lgrotrab@users.noreply.github.com>
2023-07-13 11:26:01 +0000

    Adicionando página inicial com mensagem

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return (
+    <div>
+        <h1>Não vou te ver em São Paulo seu corno ❤️</h1>
+        <h2>Bora vender only fans de IA para tirar dinheiro de trouxa</h2>
+    </div>);
+}
+
+export default Home;
