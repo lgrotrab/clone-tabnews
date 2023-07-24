25d31d9b0ea76eafec2940bb2f7f3f2498fa0ce7
Luis Gustavo Rodrigues Oliveira <54413345+lgrotrab@users.noreply.github.com>
2023-07-24 14:08:37 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-function Home(){
-    return (
+function Home() {
+  return (
     <div>
-        <h1>Não vou te ver em São Paulo seu corno ❤️</h1>
-        <h2>Bora vender only fans de IA para tirar dinheiro de trouxa</h2>
-    </div>);
+      <h1>Não vou te ver em São Paulo seu corno ❤️</h1>
+      <h2>Bora vender only fans de IA para tirar dinheiro de trouxa</h2>
+    </div>
+  );
 }
 
 export default Home;
