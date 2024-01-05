56caf21db0fac84eaefdb42e4b830c4513520ba1
Luis Gustavo Rodrigues Oliveira <54413345+lgrotrab@users.noreply.github.com>
2024-01-05 14:07:31 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const versionQueryResult = await database.query("SHOW server_version;");
+  const dbVersion = versionQueryResult.rows[0].server_version;
+
+  const maxConnectionsQueryResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const maxConnections = maxConnectionsQueryResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const currentConnectionsQueryResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+  const currentConnections = currentConnectionsQueryResult.rows[0].count;
+
   response.status(200).json({
-    message: "Os alunos do curso.dev são acima da média",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(maxConnections),
+        current_connections: currentConnections,
+      },
+    },
   });
 }
 
