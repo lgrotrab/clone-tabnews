8d3dcceb3df4ae65760f53ec25544068e6fc2769
Luis Gustavo Rodrigues Oliveira <54413345+lgrotrab@users.noreply.github.com>
2024-05-10 12:41:22 +0000

    Changes `migrations/index.js` to end not accepted method connections and resolves 504 timeout bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -12,21 +12,27 @@ export default async function migrations(request, response) {
     verbose: true,
     migrationsTable: "pgmigrations",
   };
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+  try {
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      await dbClient.end();
+      return response.status(200).json(pendingMigrations);
     }
-    return response.status(200).json(migratedMigrations);
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+      await dbClient.end();
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
+    }
+    return response.status(405).end();
+  } catch (error) {
+    return response.status(500).json({ error: error.message });
+  } finally {
+    await dbClient.end();
   }
-  return response.status(405);
 }
